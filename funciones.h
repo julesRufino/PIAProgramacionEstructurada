177d089a0c012c69ddcbb7ccb59138585cd7b9ad
AdrianMtzAlv <adrimtzalv@gmail.com>
2025-11-07 16:31:25 -0600

    actualizacion

--- funciones.h
+++ funciones.h
@@ -1,8 +1,30 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+#include <stdio.h>
+
+
 void imprimirMenu();
 void imprimirSubMenu(char *titulo);
 void imprimirMenuReportes();
-void menuReportes(char);
+void menuReportes(char opcionF);
+
+
+void AgregarCliente(FILE *fptr);
+void ConsultarCliente(FILE *fptr);
+void ModificarCliente(FILE *fptr);
+void BorrarCliente(FILE *fptr);
+
+
+void AgregarEmpleado(FILE *fptr);
+void ConsultarEmpleado(FILE *fptr);
+void ModificarEmpleado(FILE *fptr);
+void BorrarEmpleado(FILE *fptr);
+
+
+void AgregarServicio(FILE *fptr);
+void ConsultarServicio(FILE *fptr);
+void ModificarServicio(FILE *fptr);
+void BorrarServicio(FILE *fptr);
+
 #endif
