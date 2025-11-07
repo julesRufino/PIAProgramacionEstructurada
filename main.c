25fd984831a137c0fd40ddf18bb4b2d148db9317
AdrianMtzAlv <adrimtzalv@gmail.com>
2025-11-07 10:59:49 -0600

    actualizacion

--- main.c
+++ main.c
@@ -29,6 +29,24 @@ main()
                 printf("Opcion: ");
                 scanf(" %c", &subopcion);
                 //ValidarSubmenu(&subopcion);
+                switch(subopcion)
+                {
+                    case 'A':
+                        //AgregarCliente(fClientes);
+                        break;
+                    case 'B':
+                        //ConsultarCliente(fClientes);
+                        break;
+                    case 'M':
+                        //ModificarCliente(fClientes);
+                        break;
+                    case 'D':
+                        //BorrarCliente(fClientes);
+                        break;
+                    case 'S':
+                        break;
+                }
+
                 break;
     
             case 2:
@@ -36,6 +54,24 @@ main()
                 printf("Opcion: ");
                 scanf(" %c", &subopcion);
                 //ValidarSubmenu(&subopcion);
+                switch(subopcion)
+                {
+                    case 'A':
+                        //AgregarEmpleado(fEmpleados);
+                        break;
+                    case 'B':
+                        //ConsultarEmpleado(fEmpleados);
+                        break;
+                    case 'M':
+                        //ModificarEmpleado(fEmpleados);
+                        break;
+                    case 'D':
+                        //BorrarEmpleado(fEmpleados);
+                        break;
+                    case 'S':
+                        break;
+                }
+
                 break;
 
             case 3:
@@ -43,6 +79,23 @@ main()
                 printf("Opcion: ");
                 scanf(" %c", &subopcion);
                 //ValidarSubmenu(&subopcion);
+                switch(subopcion)
+                {
+                    case 'A':
+                        //AgregarServicio(fServicios);
+                        break;
+                    case 'B':
+                        //ConsultarServicio(fServicios);
+                        break;
+                    case 'M':
+                        //ModificarServicio(fServicios);
+                        break;
+                    case 'D':
+                        //BorrarServicio(fServicios);
+                        break;
+                    case 'S':
+                        break;
+                }
                 break;
 
             case 4:
@@ -50,6 +103,23 @@ main()
                 printf("Opcion: ");
                 scanf(" %c", &subopcion);
                 //ValidarSubmenu(&subopcion);
+                switch(subopcion)
+                {
+                    case 'A':
+                        //AgregarCita(fAgenda);
+                        break;
+                    case 'B':
+                        //ConsultarCita(fAgenda);
+                        break;
+                    case 'M':
+                        //ModificarCita(fAgenda);
+                        break;
+                    case 'D':
+                        //BorrarCita(fAgenda);
+                        break;
+                    case 'S':
+                        break;
+                }
                 break;
 
             case 5:
